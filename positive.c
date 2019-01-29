ca0ae85e2b149846b51cd03da841c7fd33c74c1a
mathushri <46916374+mathushri@users.noreply.github.com>
2019-01-29 12:05:47 +0530

    Update and rename README.md to positive.c

---
+++ positive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter the number":\n);
+scanf("%d",&n);
+if(n>o)
+printf("it is a positive number");
+if(n<0)
+printf("it is a negative number");
+else
+printf("it is neither positive nor negative");
+getch();
+}
