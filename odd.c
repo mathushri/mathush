86e8ddca11195e7ba7102fc0bb33f886ebb9b1b4
mathushri <46916374+mathushri@users.noreply.github.com>
2019-01-29 12:17:42 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter the number":\n);
+scanf("%d",&n);
+if(n is divisible by 2)
+printf("the number is even");
+else
+if(n is not divisible by 2)
+printf("the number is odd");
+getch();
+}
